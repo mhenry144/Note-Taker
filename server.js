48d4b606b0d01fde61002b8b25f0dfb582ba6b18
Matthew Henry <tadaemusic@gmail.com>
2020-05-23 14:45:14 -0400

    dependencies

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || "3000");
 
 var app = express();
 
-
-// connection to express server using our port number
-app.listen(app.get('port'));
+// start the server at localhost:3000
+app.listen(app.get("port"));
+console.log("Server started! At http://localhost:" + port);
